c9c52d7c0bdab1b8710a5fa946de1f380acb4b69
Juan Felipe <155260142+Felipe1727@users.noreply.github.com>
2024-12-02 18:26:22 -0500

    Pseudocode of the getComputerChoice function

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+    //GET a random number within 1 and 3 and store it into a variable
+    //IF random number
+        //IS 1 -> RETURN "rock"
+        //IS 2 -> RETURN "paper"
+        //IS 3 -> RETURN "scissors"
+}
