309e6f9fc072701e94a1b5ce87b60859aebbcb6e
Juan Felipe <155260142+Felipe1727@users.noreply.github.com>
2024-12-02 18:30:02 -0500

    Translation of pseudocode of the getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
 function getComputerChoice() {
-    //GET a random number within 1 and 3 and store it into a variable
-    //IF random number
-        //IS 1 -> RETURN "rock"
-        //IS 2 -> RETURN "paper"
-        //IS 3 -> RETURN "scissors"
+    const randomNum = Math.floor(Math.random * 3) + 1;
+    switch (randomNum) {
+        case 1:
+            return "rock";
+        case 2:    
+            return "paper"
+        case 3:
+            return "scissors";
+    }
 }
