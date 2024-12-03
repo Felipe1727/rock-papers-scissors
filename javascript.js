cef5058f315801a5e7bba4af950c8d53a5f0485a
Juan Felipe <155260142+Felipe1727@users.noreply.github.com>
2024-12-02 21:49:06 -0500

    Code getCurrentScore function & Fix playRound

--- javascript.js
+++ javascript.js
@@ -2,21 +2,37 @@ let humanScore = 0;
 let computerScore = 0;
 
 function playRound(computerChoice = getComputerChoice(), humanChoice = getHumanChoice()) {
+    if(humanChoice === computerChoice) {
+        console.log("It's a tie!");
+        console.log(getCurrentScore());
+    }
     switch(`${humanChoice + computerChoice}`) {
         //IF human wins
-        case  "rock" + "scissors" || "paper" + "rock" || "rock" + "scissors":
+        case "rock" + "scissors": 
+        case "paper" + "rock": 
+        case "scissors" + "paper":
             humanScore ++;
             console.log(`You win! ${humanChoice} beats ${computerChoice}`);
             //TO-DO function of global score
-            console.log();
+            console.log(getCurrentScore());
             break;
         //IF computer wins
-        case "rock" + "paper" || "paper" + "scissors" || "scissors" + "rock":
+        case "rock" + "paper": 
+        case "paper" + "scissors": 
+        case "scissors" + "rock":
             computerScore ++;
             console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-            console.log();
+            console.log(getCurrentScore());
             break;
     }
+}
+
+function getCurrentScore() {
+    console.log("The current score is:");
+    if (computerScore > humanScore){
+        return `Computer: ${computerScore}  -  Human: ${humanScore}`;
+    }
+    return `Human: ${humanScore}  -  Computer: ${computerScore}`;
 }
 
 function getComputerChoice() {
